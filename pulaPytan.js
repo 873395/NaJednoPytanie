cb715d23502c4c6573f0a5765c56b5ff21a50e13
Kubasiak1 <75041234+Kubasiak1@users.noreply.github.com>
2021-04-24 16:08:40 +0200

    v2.0

--- pulaPytan.js
+++ pulaPytan.js
@@ -2,8 +2,14 @@ let iloscDivow=1;
 let iloscOdpowiedzi=3;
 let numerPytania=1;
 let numerDoPytan=new Array();
+let odnosnikPytania=new Array();
+let tablicaOdpowiedzi=new Array();
+let sumaPunktow=0;
 let moznoKlikalnoscPlus=true;
 let moznoKlikalnoscMinus=false;
+let nmrPojemnika;
+let nmrOdpowiedzi;
+let wyslaneOdpowiedzi=false;
 function liczbaPytan(numer) //sprawdzanie panelu po lewej
 {
  if(numer==1&&moznoKlikalnoscPlus){iloscDivow=iloscDivow+1};
@@ -51,6 +57,9 @@ else
 
 function kreatorElementow() //tworzenie divów
 {
+    wyslaneOdpowiedzi=false;
+    tablicaOdpowiedzi=[];
+    odnosnikPytania=[];
     let k=iloscDivow;
     let n=pytania.length-1; 
     let numbers = new Array(n);
@@ -67,8 +76,9 @@ function kreatorElementow() //tworzenie divów
     numerPytania=1;
         for(let q=0;q<numerDoPytan.length;q++)
     {   
+        tablicaOdpowiedzi.push(0);
         let glownyDiv=document.createElement('div');
-        glownyDiv.id=`pojemnikk`;
+        glownyDiv.id=`pojemnik${q}`;
         glownyDiv.className='glownyDiv';
 
         let identyfikatorPytania=document.createElement('div');
@@ -86,19 +96,75 @@ function kreatorElementow() //tworzenie divów
             for(let a=1;a<=iloscOdpowiedzi;a++)
             {
                 let odpowiedzi=document.createElement('div');
-                odpowiedzi.id=`odpowiedz${a}`;
+                odpowiedzi.id=`odpowiedz${q}${a}`;
                 odpowiedzi.className='odpowiedzi';
-                odpowiedzi.addEventListener('click',()=>{klikalnik(`${a}`);},false);
+                odpowiedzi.addEventListener('click',()=>{klikalnik(`${q}`,`${a}`);},false);
                 odpowiedzi.innerHTML=pytania[numerDoPytan[q]][a];
                 glownyDiv.appendChild(odpowiedzi);
             };
         numerPytania++;
         poleNaPytania.appendChild(glownyDiv);
     }// znacznik konca fora
+    odnosnikPytania=numerDoPytan;
     numerDoPytan=[];
+    let divSprawdzaj=document.createElement('div');
+            divSprawdzaj.className='latajacyDiv';
+            divSprawdzaj.innerHTML="sprawdź";
+            divSprawdzaj.addEventListener('click',sprawdzanieOdpowiedzi);
+            poleNaPytania.appendChild(divSprawdzaj);
 } //znacznik konca funkcji
 
-function klikalnik(niewiem)
+function klikalnik(nmrPojemnika,nmrOdpowiedzi)
 {
-    if(niewiem==1)console.warn(niewiem);
+    if(wyslaneOdpowiedzi)return;
+    const a=document.getElementById(`odpowiedz${nmrPojemnika}${1}`);
+    const b=document.getElementById(`odpowiedz${nmrPojemnika}${2}`);
+    const c=document.getElementById(`odpowiedz${nmrPojemnika}${3}`);
+        [a,b,c].forEach(i=>{i.style.border=''});
+    
+    const odp=document.getElementById(`odpowiedz${nmrPojemnika}${nmrOdpowiedzi}`);
+    odp.style.borderBottom='3px solid orange';
+        odp.style.borderLeft='3px solid orange';
+    tablicaOdpowiedzi[nmrPojemnika]=nmrOdpowiedzi;
+    console.warn(tablicaOdpowiedzi);
 }
+function sprawdzanieOdpowiedzi()
+{
+    wyslaneOdpowiedzi=true;
+
+    for(let m=0;m<iloscDivow;m++)
+    {
+        if(tablicaOdpowiedzi[m]==0)continue;
+        const dobra=document.getElementById(`odpowiedz${m}${tablicaOdpowiedzi[m]}`);
+            let A;
+             if(tablicaOdpowiedzi[m]==1){A='A'};
+             if(tablicaOdpowiedzi[m]==2){A='B'};
+             if(tablicaOdpowiedzi[m]==3){A='C'};
+             if(pytania[odnosnikPytania[m]][4]==A){
+                dobra.style.borderBottom='4px solid green';
+                dobra.style.borderLeft='4px solid green';
+                sumaPunktow++;
+             }
+             else
+             {
+                dobra.style.borderBottom='4px solid red';
+                dobra.style.borderLeft='4px solid red';
+             }  
+    }//koniec
+    opcja2.innerHTML=`Zdobyłeś ${sumaPunktow} / ${iloscDivow}`;
+}
+
+
+
+
+
+// let A;
+  // if(nmrOdpowiedzi==1){A='A'};
+  // if(nmrOdpowiedzi==2){A='B'};
+  // if(nmrOdpowiedzi==3){A='C'};
+  // if(pytania[odnosnikPytania[nmrPojemnika]][4]==A){
+  //     pOdp.style.backgroundColor='green';
+  //     sumaPunktow++;
+  //        
+  //             console.warn(sumaPunktow);
+  //  
